9fca848342c59b210a024e286cc74842a7c5ebe1
apokalipsa44 <m_lachowicz@wp.pl>
2021-02-24 23:44:08 +0100

    last styles done

--- JS/memory.js
+++ JS/memory.js
@@ -1,4 +1,7 @@
-const cards = document.querySelectorAll(".card");
+let cards = document.querySelectorAll(".card");
+const resetButton = document.getElementById("reset-button");
+const revealedCards = document.getElementById("found");
+const userMessage = document.getElementById("user-message");
 console.log("cards: ", cards);
 
 let firstCard;
@@ -7,15 +10,29 @@ let isCardFlipped = false;
 let score = 0;
 let time = 0;
 let blockMoves = false;
+let moves = 0;
 
-function flipCard(card) {
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    startRound(card);
+  });
+});
+
+reRenderCards();
+
+resetButton.addEventListener("click", () => {
+  resetGame();
+});
+
+function startRound(card) {
   console.log(blockMoves);
   if (blockMoves) return;
-  console.log("card: ", card);
   initCounter();
   card.classList.add("flip");
 
   if (!isCardFlipped) {
+    moves++;
+    updateTraysScore();
     isCardFlipped = true;
     firstCard = card;
     return;
@@ -24,19 +41,50 @@ function flipCard(card) {
   blockMoves = true;
   checkCards(firstCard, secondCard);
 }
+function resetGame() {
+  time = 0;
+  score = 0;
+  moves = 0;
+  blockMoves = false;
+  reRenderCards();
+  updateDisplayScore();
+  updateTraysScore();
+  clearRevealedCards();
+  clearUserMessage();
+}
+
+function reRenderCards() {
+  shuffle();
+  cards.forEach((card) => {
+    card.classList.remove("flip");
+    setTimeout(() => {
+      card.classList.remove("invisible");
+    }, 200);
+  });
+}
+
+function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+}
 
 function checkCards(firstCard, secondCard) {
   isCardFlipped = false;
   let isCardMatching = firstCard.dataset.muppet === secondCard.dataset.muppet;
   if (isCardMatching) {
-      score++;
+    score++;
+    updateRevealedCards(firstCard);
+    updateDisplayScore();
     setTimeout(() => {
       disableCards(firstCard, secondCard);
     }, 1000);
   }
-  if (!isCardMatching) unFlipCards(firstCard, secondCard);
+  if (!isCardMatching) unstartRounds(firstCard, secondCard);
   isCardMatching = false;
-  console.log("score: ", score);
+  blockMoves = checkForFailure();
+  checkForWining();
 }
 
 function disableCards(firstCard, secondCard) {
@@ -47,19 +95,52 @@ function disableCards(firstCard, secondCard) {
   secondCard.classList.add("invisible");
 }
 
-function unFlipCards(firstCard, secondCard) {
+function unstartRounds(firstCard, secondCard) {
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-    blockMoves = false;
+    blockMoves = checkForFailure();
   }, 1000);
 }
 
-cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    flipCard(card);
-  });
-});
+function updateDisplayScore() {
+  document.getElementById("score").innerHTML = score;
+}
+function updateTraysScore() {
+  document.getElementById("moves").innerHTML = moves;
+}
+
+function updateRevealedCards(firstCard) {
+  const revealedImage = document.createElement("img");
+  revealedImage.src = firstCard.children[0].src;
+  revealedImage.classList.add("revealed-image");
+  setTimeout(() => {
+    revealedCards.appendChild(revealedImage);
+  }, 1500);
+}
+
+function clearRevealedCards() {
+  while (revealedCards.firstChild) {
+    revealedCards.firstChild.remove();
+  }
+}
+
+function checkForFailure() {
+  if (moves > 15) {
+    userMessage.innerHTML = "So sad...You lost!";
+    return true;
+  }
+  return false;
+}
+
+function checkForWining() {
+  console.log("score", score);
+  if (score == 6) userMessage.innerHTML = "Congratulations! You won!";
+}
+
+function clearUserMessage() {
+  userMessage.innerHTML = "";
+}
 
 function initCounter(val) {
   return val > 9 ? val : "0" + val;
